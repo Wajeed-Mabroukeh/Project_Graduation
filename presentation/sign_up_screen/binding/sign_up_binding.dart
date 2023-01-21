2149dd1e461e8e5a68316f29699da55ace3ccda9
Wajeed Mabroukeh <57049753+Wajeed-Mabroukeh@users.noreply.github.com>
2023-01-21 22:08:30 +0200

    Delete presentation/sign_up_screen directory

--- presentation/sign_up_screen/binding/sign_up_binding.dart
+++
@@ -1,9 +0,0 @@
-import '../controller/sign_up_controller.dart';
-import 'package:get/get.dart';
-
-class SignUpBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => SignUpController());
-  }
-}
